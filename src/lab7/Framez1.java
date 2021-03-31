ba1bee27ba9f0f049e41e86958603dd49724ce39
sysadmin <sysadmin@bcalab5-m14>
2021-03-31 13:20:03 +0530

    functions but combo and button missing

---
+++ src/lab7/Framez1.java
@@ -0,0 +1,138 @@
+package lab7;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.*;
+
+public class Framez1 {
+
+	private JFrame frame;
+	private JTextField t1;
+	private JTextField t2;
+	private JTextField t3;
+	String name,address,phno;
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Framez1 window = new Framez1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Framez1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Name");
+		lblNewLabel.setBounds(59, 45, 124, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		t1 = new JTextField();
+		t1.setBounds(254, 42, 86, 20);
+		frame.getContentPane().add(t1);
+		t1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Address");
+		lblNewLabel_1.setBounds(59, 101, 144, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		t2 = new JTextField();
+		t2.setBounds(254, 98, 86, 20);
+		frame.getContentPane().add(t2);
+		t2.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Phone Number");
+		lblNewLabel_2.setBounds(59, 160, 157, 14);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		t3 = new JTextField();
+		t3.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				
+				char c = arg0.getKeyChar();
+				String phone = t3.getText();
+				
+				if(Character.isDigit(c) || Character.isISOControl(c))
+				{
+					t3.setEditable(true);
+				}
+		else
+		{
+			t3.setEditable(false);
+			JOptionPane.showMessageDialog(frame, " Phone number must contain only digits");
+		}
+		
+		}
+		});
+		t3.setBounds(254, 157, 86, 20);
+		frame.getContentPane().add(t3);
+		t3.setColumns(10);
+		
+		JButton submit = new JButton("CLICK");
+		submit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				name=t1.getText();
+				address=t2.getText();
+				phno=t3.getText();
+				
+				if(name.contentEquals(""))
+				{
+					JOptionPane.showMessageDialog(frame, "name cannot be empty");
+				}
+				/*
+				else
+				{
+					t1.setEditable(false);
+					JOptionPane.showMessageDialog(frame, "Phone number must contain only digits");
+				}
+				*/
+				if(phno.length()>10)
+				{
+					t3.setEditable(false);
+					JOptionPane.showMessageDialog(frame, "Phone number must contain only 10 digits");
+					t3.setEditable(true);
+				}
+				else
+				{
+					t3.setEditable(true);
+				}
+				
+				Framez1display fobj = new Framez1display(name,address,phno);
+				
+			}
+		});
+		submit.setBounds(160, 210, 89, 23);
+		frame.getContentPane().add(submit);
+	}
+}
---
+++ src/lab7/Framez1display.java
@@ -0,0 +1,149 @@
+package lab7;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Framez1display {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JButton backbtn;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Framez1display window = new Framez1display();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Framez1display() {
+		initialize();
+	}
+	
+	public Framez1display(String name1,String address1,String phno1) {
+		initialize(name1,address1,phno1);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	
+	private void initialize(String name1,String address1,String phno1) {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setEnabled(false);
+		textField.setEditable(false);
+		textField.setBounds(238, 34, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		textField.setText(name1);
+		frame.setVisible(true);
+		
+		textField_1 = new JTextField();
+		textField_1.setEnabled(false);
+		textField_1.setEditable(false);
+		textField_1.setBounds(238, 100, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		textField_1.setText(address1);
+		frame.setVisible(true);
+		
+		textField_2 = new JTextField();
+		textField_2.setEnabled(false);
+		textField_2.setEditable(false);
+		textField_2.setBounds(238, 172, 86, 20);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		textField_2.setText(phno1);
+		frame.setVisible(true);
+		
+		JLabel lblNewLabel = new JLabel("Name");
+		lblNewLabel.setBounds(48, 37, 143, 17);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Address");
+		lblNewLabel_1.setBounds(48, 103, 161, 17);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Phone Number");
+		lblNewLabel_2.setBounds(48, 175, 190, 17 );
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		backbtn = new JButton("BACK");
+		backbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				frame.setVisible(false);
+			}
+		});
+		backbtn.setBounds(150, 216, 89, 23);
+		frame.getContentPane().add(backbtn);
+	}
+	
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setEnabled(false);
+		textField.setEditable(false);
+		textField.setBounds(273, 34, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setEnabled(false);
+		textField_1.setEditable(false);
+		textField_1.setBounds(273, 100, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setEnabled(false);
+		textField_2.setEditable(false);
+		textField_2.setBounds(273, 172, 86, 20);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Name");
+		lblNewLabel.setBounds(48, 37, 143, 17);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Address");
+		lblNewLabel_1.setBounds(48, 103, 161, 17);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Phone Number");
+		lblNewLabel_2.setBounds(48, 175, 190, 17);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		
+	}
+
+}
